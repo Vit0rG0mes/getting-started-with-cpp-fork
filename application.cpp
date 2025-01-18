864694a9837fc5e9003015ed764a070de454affc
jstoquica <37915158+jstoquica@users.noreply.github.com>
2025-01-18 12:19:35 -0300

    Add files via upload

---
+++ application.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "myheader.h"
+
+int main()
+{
+	int a = 0;
+	std::cin >> a;
+	std::cout << myfunc(a);
+       
+	return 0;
+}
---
+++ myheader.h
@@ -0,0 +1,12 @@
+// Declaração de funções e classes autodefinidas
+// Função chamada myfunc() que recebe dados int, sendo tipo long
+
+#ifndef _MYHEADER_
+#define _MYHEADER_
+
+long myfunc(int num)
+{
+	return num + num;
+}
+
+#endif
